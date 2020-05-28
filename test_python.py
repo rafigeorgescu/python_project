cdd271fa83efca6f8a09e41654c96451acc30379
Rafaela Georgescu <rafaela.georgescu@accesa.eu>
2020-05-28 19:30:25 +0300

    third commit

--- test_python.py
+++ test_python.py
@@ -1,3 +1,22 @@
 print("Hello, World!")
 
-print("New line")
+# one_variable = '5'
+# some_string='some other string'
+
+# third_variable = one_variable + some_string
+
+# some_number = 5
+# some_other_number = 6
+
+# rwsult = some_number + some_other_number
+
+# items = ['1', '2', '3', '4', '5']
+
+# for item in items:
+#     print(item)
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
